374fe13a9408f2250c7b95510b2bdf4e1bbb45de
Dylan Mooers <dylmooers@gmail.com>
2022-05-05 07:44:47 -0700

    util functions for accessing async storage

---
+++ src/utils/asyncStorage.ts
@@ -0,0 +1,36 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const asyncWriteString = async (key: string, value: string) => {
+  try {
+    await AsyncStorage.setItem(key, value);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const asyncWriteObject = async (
+  key: string,
+  value: { [field: string]: any }
+) => {
+  try {
+    const json = JSON.stringify(value);
+    await AsyncStorage.setItem(key, json);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const asyncReadString = async (key: string): Promise<string | null> => {
+  try {
+    const value = await AsyncStorage.getItem(key);
+    return value;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+};
+
+export const asyncReadObject = async <T>(key: string): Promise<T | null> => {
+  const jsonString = await asyncReadString(key);
+  return jsonString ? JSON.parse(jsonString) : null;
+};

a84ce1b928724901f55e7ebca279c74dc45dc682
Dylan Mooers <dylmooers@gmail.com>
2022-05-31 23:57:22 -0700

    detect ios emulator

--- src/utils/storageKeys.ts
+++ src/utils/storageKeys.ts
@@ -4,6 +4,8 @@ import {
   AUTHENTICATION_TYPE,
   BIOMETRY_TYPE,
 } from 'react-native-keychain';
+import DeviceInfo from 'react-native-device-info';
+import { Platform } from 'react-native';
 export const WALLET_KEY_SUFFIX = '/Wallet/Info';
 export const DEFAULT_PREFIX = '@node-fi/sdk';
 export const MNEMONIC = '/secret';
@@ -12,11 +14,20 @@ export const SECURE_ENCLAVE_LABEL = '0xNodeFiSecureEnclave';
 export const KEYCHAIN_SETTINGS = (
   service: string,
   biometryType: BIOMETRY_TYPE | null
-) => ({
-  accessControl: biometryType
-    ? ACCESS_CONTROL.BIOMETRY_CURRENT_SET
-    : ACCESS_CONTROL.USER_PRESENCE,
-  accessible: ACCESSIBLE.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
-  authenticationType: AUTHENTICATION_TYPE.DEVICE_PASSCODE_OR_BIOMETRICS,
-  service,
-});
+) => {
+  if (DeviceInfo.isEmulatorSync() && Platform.OS === 'ios') {
+    console.warn(
+      'iOS Emulator detected, disabling Biometrics for this session.  If you are not on an emulator contact dylan@thenode.fi immediately to report this bug.'
+    );
+    return { service };
+  }
+
+  return {
+    accessControl: biometryType
+      ? ACCESS_CONTROL.BIOMETRY_CURRENT_SET
+      : ACCESS_CONTROL.USER_PRESENCE,
+    accessible: ACCESSIBLE.WHEN_UNLOCKED_THIS_DEVICE_ONLY,
+    authenticationType: AUTHENTICATION_TYPE.DEVICE_PASSCODE_OR_BIOMETRICS,
+    service,
+  };
+};
